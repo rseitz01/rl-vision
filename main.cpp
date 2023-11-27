4749fa24176b306bb02c93f42d4deee9fd6c3166
Raphael Seitz <r.seitz2001@gmail.com>
2023-11-27 18:56:15 +0100

    add more things, now shows fps and frame counter

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include <stdio.h>
 
 #include <ueye.h>
-#include <uEye_tools.h>
+//#include <uEye_tools.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include "Ueye.h"
+//#include "Ueye.h"
 
+#include <vector>
+#include <chrono>
 
 #if 1
 
 #define CHAR_ESC 27
 
+
+
 int main(void)
 {
-#if 1
+#if 0
     auto ueye = Ueye::Ueye();
 
     if (ueye.connect()) return -1;
@@ -30,10 +34,12 @@ int main(void)
 
     HIDS hCam = { 0 }; // IDS VideoCapture Device
     int exp_time = 50; // Belichtungszeit
-     CAMINFO camInfo; // Kamera-Daten
-     SENSORINFO sensorInfo; // Sensor-Daten
-     char* imgMem; // Zeiger auf reservierten Speicherbereich für das Bild
-     int memId; // ID für den Bildspeicher (mehrere Bildspeicher sind möglich)
+    CAMINFO camInfo; // Kamera-Daten
+    SENSORINFO sensorInfo; // Sensor-Daten
+    std::vector<char*>imgMems;
+    std::vector<int>memIds;
+    //char* imgMem; // Zeiger auf reservierten Speicherbereich für das Bild
+    //int memId; // ID für den Bildspeicher (mehrere Bildspeicher sind möglich)
     char* errstr = 0;
 
     if (is_InitCamera(&hCam, NULL) == IS_SUCCESS) {
@@ -55,9 +61,11 @@ int main(void)
     is_SetColorMode(hCam, nColorMode);
     int img_width = 1600;
     int img_height = 1200;
+#if 0 //do this below
     is_AllocImageMem(hCam, img_width, img_height, nBitsPerPixel, &imgMem, &memId);
     is_SetImageMem(hCam, imgMem, memId);
     is_SetDisplayMode(hCam, IS_SET_DM_DIB);
+#endif
 
     // parameters
     double exposure = (double)exp_time;
@@ -87,6 +95,27 @@ int main(void)
     //is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_GAIN, &disable, 0);
     //is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_WHITEBALANCE, &disable, 0);
     //is_SetAutoParameter(hCam, IS_SET_ENABLE_AUTO_SENSOR_SHUTTER, &disable, 0);
+
+#if 0
+    UINT nNumberOfSupportedPixelClocks = 0;
+    nRet = is_PixelClock(hCam, IS_PIXELCLOCK_CMD_GET_NUMBER,
+        (void*)&nNumberOfSupportedPixelClocks,
+        sizeof(nNumberOfSupportedPixelClocks));
+    if ((nRet == IS_SUCCESS) && (nNumberOfSupportedPixelClocks > 0))
+    {
+        // No camera has more than 150 different pixel clocks.
+        // Of course, the list can be allocated dynamically
+        UINT nPixelClockList[150];
+        ZeroMemory(&nPixelClockList, sizeof(nPixelClockList));
+
+        nRet = is_PixelClock(hCam, IS_PIXELCLOCK_CMD_GET_LIST,
+            (void*)nPixelClockList,
+            nNumberOfSupportedPixelClocks * sizeof(UINT));
+        for (int i = 0; i < nNumberOfSupportedPixelClocks; i++) {
+            printf("pixelclock : %d\n", nPixelClockList[i]);
+        }
+    }
+#endif
 
     // set parameters
     int error = IS_SUCCESS;
@@ -139,6 +168,39 @@ int main(void)
     }
 
 
+
+    for (double f = 0; f < 1.0; f += 1.0 / fps) {
+        size_t i = imgMems.size();
+        imgMems.resize(i + 1);
+        memIds.resize(i + 1);
+        char* imgMem = imgMems.back();
+        error = is_AllocImageMem(hCam, img_width, img_height, nBitsPerPixel, &imgMem, &memIds[i]);
+        if (error) break;
+        error = is_AddToSequence(hCam, imgMem, memIds[i]);
+        if (error) break;
+        //printf("memids[%d] = %d\n", i, memIds[i]);
+    }
+    if (error) {
+        printf("error while allocating or setting image memory (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+    if ((error = is_ImageQueue(hCam, IS_IMAGE_QUEUE_CMD_INIT, 0, 0))) {
+        printf("could not enable image queue (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+
+    if ((error = is_SetDisplayMode(hCam, IS_SET_DM_DIB))) {
+        printf("could not set display mode (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+    // setLongExposure bei > 80 irgendwas?
+
+
     // Save parameters to file (open filebox)
     //nRet = is_ParameterSet(hCam, IS_PARAMETERSET_CMD_SAVE_FILE, NULL, NULL);
 
@@ -147,6 +209,7 @@ int main(void)
 
 
     // is_AddToSequence
+    // is_FreezeVideo
     // is_GetImageInfo (timestamps)
     // nMemId / dwImageBuffers 
 
@@ -162,19 +225,127 @@ int main(void)
 
     // e)
     // is_CaptureVideo
+
+    if ((error = is_SetExternalTrigger(hCam, IS_SET_TRIGGER_OFF))) {
+        printf("could not disable external trigger (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+    if ((error = is_CaptureVideo(hCam, IS_DONT_WAIT))) {
+        printf("could not enable video capture (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+    //clock_t frame_t_last = clock();
+    //clock_t frame_t_now = clock();
+    uint64_t drop_frames = 0;
+    uint64_t frame_count = 0;
+    std::chrono::steady_clock::time_point chr_now = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point chr_prev;
+    cv::String fps_str = "FPS : ...";
+   
     while (1) {
-        if (is_FreezeVideo(hCam, IS_WAIT) == IS_SUCCESS) {
+#if 1
+        char* pcMem = 0;
+        int nMemId = 0;
+        is_GetImageMem(hCam, (void **) &pcMem);
+        is_LockSeqBuf(hCam, 0, pcMem);
+
+        is_UnlockSeqBuf(hCam, 0, pcMem);
+
+        unsigned int pending = 0;
+        if ((error = is_ImageQueue(hCam, IS_IMAGE_QUEUE_CMD_GET_PENDING, &pending, sizeof(pending)))) {
+            printf("could not get pending image queue (%d)\n", error);
+            is_GetError(hCam, &lastError, &errstr);
+            printf("error %d: %s\n", lastError, errstr);
+        }
+        if (pending) {
+
+            //if (first_tp == std::chrono::steady_clock::time_point{}) {
+            //first_tp = std::chrono::steady_clock::now();
+            //}
+            //frame_t_last = frame_t_now;
+            //frame_t_now = clock();
+            //clock_t frame_t_delta = frame_t_now - frame_t_last;
+            
+            chr_now = std::chrono::steady_clock::now();
+            frame_count++;
+            if (chr_now - chr_prev >= std::chrono::seconds{1}) {
+                /* Do something with the fps in frame_counter */
+                long long frame_delta = (std::chrono::duration_cast<std::chrono::nanoseconds>(chr_now - chr_prev)).count();
+                double fps_actual = frame_delta ? 1.0e9 / (double)frame_delta * frame_count : 0;
+                char fps_str_buf[32] = { 0 };
+                std::snprintf(fps_str_buf, 32, "%.2f", fps_actual);
+                fps_str = cv::String("FPS : " + std::string(fps_str_buf));
+                chr_prev = chr_now;
+                frame_count = 0;
+            }
+
+            auto frame = cv::Mat(img_height, img_width, CV_8UC1, pcMem);
+            auto drop_frames_str = cv::String("Drop Frames : " + std::to_string(drop_frames));
+
+            cv::putText(frame, drop_frames_str, cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 2);
+            cv::putText(frame, fps_str, cv::Point(10, 100), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 2);
+            cv::imshow("frame", frame);
+
+
+            drop_frames += (pending - 1);
+            printf("pending : %d\n", pending);
+            if ((error = is_ImageQueue(hCam, IS_IMAGE_QUEUE_CMD_DISCARD_N_ITEMS, &pending, sizeof(pending)))) {
+                printf("could not discard n items in image queue (%d)\n", error);
+                is_GetError(hCam, &lastError, &errstr);
+                printf("error %d: %s\n", lastError, errstr);
+            }
+        }
+#else
+        //if (is_FreezeVideo(hCam, IS_WAIT) == IS_SUCCESS) {
             void* pMemVoid; //pointer to where the image is stored
             is_GetImageMem(hCam, &pMemVoid);
             auto frame = cv::Mat(img_height, img_width, CV_8UC1, pMemVoid);
             //cv::addWeighted(frame, 1, 0, 0, offset, frame);
             cv::imshow("frame", frame);
         }
-        char c = cv::waitKey(1000.0 / fps);
+#endif
+        //char c = cv::waitKey((int)(1000.0 / fps));
+        char c = cv::waitKey(1);
         if (c == 'q' || c == CHAR_ESC) {
             break;
         }
     }
+
+    if ((error = is_StopLiveVideo(hCam, IS_FORCE_VIDEO_STOP))) {
+        printf("could not stop video capture (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+    if ((error = is_ClearSequence(hCam))) {
+        printf("could not stop video capture (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+    for (size_t i = 0; i < imgMems.size(); i++) {
+        char* pcMem = imgMems[i];
+        error = is_FreeImageMem(hCam, pcMem, memIds[i]);
+        if (error) break;
+    }
+    if (error) {
+        printf("error while freeing image memory (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+
+    if ((error = is_ExitCamera(hCam))) {
+        printf("could not close camera (%d)\n", error);
+        is_GetError(hCam, &lastError, &errstr);
+        printf("error %d: %s\n", lastError, errstr);
+    }
+
+
     // is_ParameterSet (write settings to file)
 
 #endif
